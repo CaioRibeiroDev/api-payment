9d15c5a60ba9a0c65630843b15e94c8e9b5a9e0a
CaioRibeiroDev <caioribeirodev@gmail.com>
2023-04-07 16:49:57 -0300

    🎉 Begin a project

---
+++ src/http/tsyringe/index.ts
@@ -0,0 +1,10 @@
+// import { container } from 'tsyringe'
+// import { AccountRepository } from 'src/modules/account/repositories/AccountRepository'
+// import { IAccountRepository } from 'src/modules/account/repositories/IAccountRepository'
+
+/**
+ * container.registerSingleton<NameRepositoryInterface>(
+ *    'NameRepository',
+ *    NameRepository
+ *  )
+ */
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata'
+import 'express-async-errors'
+import cors from 'cors'
+import express, { NextFunction, Request, Response, urlencoded } from 'express'
+import '../src/http/tsyringe'
+import { AppError } from './errors/app-error'
+import { routes } from './routes'
+
+const app = express()
+
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
+app.use(cors())
+app.use(routes)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) return response.status(err.statusCode).json({ message: err.message })
+
+  return response.status(500).json({
+    status: 'Error',
+    message: `Internal server error - ${err.message}`
+  })
+})
+
+app.listen(3333)
